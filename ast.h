4573cc46c5733ec19d469ec7b881caa782852048
Gokul <gokul.sankar29@gmail.com>
2024-12-28 14:34:39 +0000

    Renamed "dump_ast" to "to_string"

--- ast.h
+++ ast.h
@@ -7,14 +7,14 @@ using namespace std;
 class ast_n
 {
 public:
-  string dump_ast(string prefix="") const { return ""; }
+  string to_string(string prefix="") const { return ""; }
 };
 
 template <typename T_NODE>
 class list_n : public ast_n
 {
 public:
-  string dump_ast(string prefix="") const;
+  string to_string(string prefix="") const;
   vector<T_NODE*> get_list() { return this->m_list; }
 private:
   vector<T_NODE*> m_list;
@@ -23,13 +23,13 @@ private:
 class function_definition_n : public ast_n
 {
 public:
-  string dump_ast(string prefix="") const;
+  string to_string(string prefix="") const;
 };
 
 class declaration_n : public ast_n
 {
 public:
-  string dump_ast(string prefix="") const;
+  string to_string(string prefix="") const;
 };
 
 union external_declaration_n
@@ -41,6 +41,6 @@ union external_declaration_n
 class translation_unit_n : public list_n<external_declaration_n>
 {
 public:
-  string dump_ast(string prefix="") const;
+  string to_string(string prefix="") const;
 };
 
